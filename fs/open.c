bc98df69a42b2abd9378f26292e50cc9a95051fb
RanggaWbp <ranggawbp16@gmail.com>
2026-09-16 19:34:42 +0700

    fix(ksu): drop literal CONFIG_KSU_MANUAL_HOOK guard, susfs-only now

--- fs/open.c
+++ fs/open.c
@@ -343,15 +343,10 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
-#if defined(CONFIG_KSU_MANUAL_HOOK) || defined(CONFIG_KSU_SUSFS)
-__attribute__((hot))
 #ifdef CONFIG_KSU_SUSFS
+__attribute__((hot))
 extern int ksu_handle_faccessat(int *dfd, struct filename **filename,
 				int *mode, int *flags);
-#else
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
-				int *mode, int *flags);
-#endif
 #endif
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
@@ -362,7 +357,6 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
-#if defined(CONFIG_KSU_MANUAL_HOOK) || defined(CONFIG_KSU_SUSFS)
 #ifdef CONFIG_KSU_SUSFS
 	{
 		struct filename *ksu_fname = getname(filename);
@@ -371,9 +365,6 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 			putname(ksu_fname);
 		}
 	}
-#else
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-#endif
 #endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
